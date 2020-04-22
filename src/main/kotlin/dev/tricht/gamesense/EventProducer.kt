22119ad693ef1a1c36a77aac7ef11a65c5313b61
Michael <michael@tricht.dev>
2020-04-22 22:38:20 +0200

    Username from txt.

--- src/main/kotlin/dev/tricht/gamesense/EventProducer.kt
+++ src/main/kotlin/dev/tricht/gamesense/EventProducer.kt
@@ -2,6 +2,7 @@ package dev.tricht.gamesense
 
 import dev.tricht.gamesense.model.Data
 import dev.tricht.gamesense.model.Event
+import java.io.File
 import java.time.LocalDateTime
 import java.util.*
 
@@ -9,11 +10,21 @@ class EventProducer(private val client: ApiClient, private val programmerClient:
 
     private var cacheTime: LocalDateTime? = null
     private var score = 0
+    private var username: String? = null
+
+    init {
+        var file = File("username.txt")
+        username = if (file.exists()) {
+            file.readText(Charsets.UTF_8)
+        } else {
+            "meineKACKA"
+        }
+    }
 
     override fun run() {
         if (cacheTime == null || LocalDateTime.now().isAfter(cacheTime)) {
             println("Refreshing cache")
-            val response = programmerClient.getProfile().execute()
+            val response = programmerClient.getProfile(username!!).execute()
             cacheTime = LocalDateTime.now().plusMinutes(2)
             if (response.body() != null) {
                 score = response.body()!!.user.score
--- src/main/kotlin/dev/tricht/gamesense/ProgrammerApiClient.kt
+++ src/main/kotlin/dev/tricht/gamesense/ProgrammerApiClient.kt
@@ -3,8 +3,9 @@ package dev.tricht.gamesense
 import dev.tricht.gamesense.model.Profile
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ProgrammerApiClient {
-    @GET("/api/profile/info?name=meineKACKA")
-    fun getProfile(): Call<Profile>
+    @GET("/api/profile/info")
+    fun getProfile(@Query("name") name: String): Call<Profile>
 }
