05c3be3b108024c761b32195a7df5bd880fecc15
Michael van Tricht <michael@stucomm.com>
2020-07-28 00:20:44 +0200

    Improve macOS performance.

--- src/main/kotlin/dev/tricht/gamesense/events/MacOSDataFetcher.kt
+++ src/main/kotlin/dev/tricht/gamesense/events/MacOSDataFetcher.kt
@@ -5,28 +5,44 @@ class MacOSDataFetcher : DataFetcher {
     private val volumeScript = arrayOf("osascript", "-e", "output volume of (get volume settings)")
     private val spotifyScript = arrayOf("osascript", "-e", "tell application \"System Events\" to get the name of the first window of (processes whose name is \"Spotify\")")
     private val runtime = Runtime.getRuntime()
-    private var waitTicks = 0
+    private var volumeWaitTicks = 0
+    private var lastVolumeChange = System.currentTimeMillis()
+    private var lastVolume = 0
+    private var songWaitTicks = 0
     private var lastSong: String? = null
 
     override fun getVolume(): Int {
+        if (volumeWaitTicks > 0) {
+            --volumeWaitTicks
+            if ((System.currentTimeMillis() - lastVolumeChange) > 1000) {
+                return lastVolume
+            }
+        }
         return try {
             val process = runtime.exec(volumeScript)
             process.waitFor()
-            Integer.parseInt(process.inputStream.readAllBytes().decodeToString().trim())
+            volumeWaitTicks = 20
+            val volume = Integer.parseInt(process.inputStream.readAllBytes().decodeToString().trim())
+            if (volume != lastVolume) {
+                lastVolumeChange = System.currentTimeMillis()
+            }
+            lastVolume = volume
+            volume
         } catch (e: Exception) {
             e.printStackTrace()
+            lastVolume = 0
             0
         }
     }
 
     override fun getCurrentSong(): String? {
-        if (waitTicks > 0) {
-            --waitTicks
+        if (songWaitTicks > 0) {
+            --songWaitTicks
             return lastSong
         }
         val process = runtime.exec(spotifyScript)
         process.waitFor()
-        waitTicks = 20
+        songWaitTicks = 20
         if (process.exitValue() != 0) {
             println(process.inputStream.readAllBytes().decodeToString().trim())
             lastSong = null
