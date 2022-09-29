fd13359cfc820192fb20e24842e9d78d2c37ca3f
Michael <michael@tricht.dev>
2022-09-29 21:24:18 +0200

    Fix time

--- src/main/kotlin/dev/tricht/gamesense/events/EventProducer.kt
+++ src/main/kotlin/dev/tricht/gamesense/events/EventProducer.kt
@@ -13,7 +13,7 @@ import java.util.*
 class EventProducer: TimerTask() {
     private val dataFetcher = WindowsDataFetcher()
     private var client = ApiClientFactory().createApiClient()
-    private val dateFormat = DateFormat.getTimeInstance()
+    private val dateFormat = DateFormat.getTimeInstance(0)
     private var volume: Int? = null
     private var waitTicks = 0
     private var currentSong: SongInformation? = null
